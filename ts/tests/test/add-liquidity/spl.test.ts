055e809474ca7d3997fd570d6db495429320b894
billythedummy <dhy1996@live.com.sg>
2025-06-05 20:20:38 +0800

    add all tests and fix update bug

--- ts/tests/test/add-liquidity/spl.test.ts
+++ ts/tests/test/add-liquidity/spl.test.ts
@@ -3,7 +3,7 @@ import { INF_MINT, JUPSOL_MINT, tradeExactInBasicTest } from "../../utils";
 
 const MINTS = { inp: JUPSOL_MINT, out: INF_MINT };
 
-describe("AddLiquidity jupsol test", async () => {
+describe("AddLiquidity spl test", async () => {
   /**
    * jupsol fixtures:
    * - pool cloned from mainnet in epoch 797 with data edited to change last_update_epoch to 0
