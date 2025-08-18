93256a3c6775b74a1518b70bb0720a3b0aec99e8
billythedummy <dhy1996@live.com.sg>
2025-08-18 14:15:32 +0800

    gen flatslab script

--- notes/test-fixtures.md
+++ notes/test-fixtures.md
@@ -7,7 +7,7 @@ Notes on structures and changes to test fixtures accounts.
 ### `lst-state-list`
 
 - Removed all mints except the ones we have test-fixtures accounts for
-- Re-enabled stsol trading
+- Re-enabled stsol input
 
 ### SOL Value Calculator Programs ProgramState accounts
 
---
+++ scripts/test-fixtures/scripts/gen-flatslab.ts
@@ -0,0 +1,96 @@
+/**
+ * Generates a flatslab slab PDA account using the constants specified below
+ */
+
+import {
+  address,
+  Address,
+  getAddressComparator,
+  getAddressDecoder,
+  getAddressEncoder,
+  getBase64Codec,
+  getBase64Decoder,
+  getI32Encoder,
+  lamports,
+  type Base64EncodedBytes,
+} from "@solana/kit";
+import { testFixturesAcc, writeTestFixturesAcc } from "../utils";
+import assert from "assert";
+
+const FLATSLAB_SLAB_NAME = "flatslab-slab";
+
+const ENTRY_LEN = 40;
+
+type Entry = { mint: Address; inpNanos: number; outNanos: number };
+
+// unsorted
+const ENTRIES: Array<Entry> = [
+  {
+    mint: address("jupSoLaHXQiZZTSfEWMTRRgpnyFm8f6sZdosWBjx93v"),
+    inpNanos: -1_000_000,
+    outNanos: -2_000_000,
+  },
+  {
+    mint: address("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"),
+    inpNanos: 3_000_000,
+    outNanos: 4_000_000,
+  },
+  {
+    mint: address("So11111111111111111111111111111111111111112"),
+    inpNanos: 5_000_000,
+    outNanos: 6_000_000,
+  },
+  {
+    mint: address("7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj"),
+    inpNanos: 7_000_000,
+    outNanos: 8_000_000,
+  },
+  {
+    mint: address("5oVNBeEEQvYi1cX3ir8Dx5n1P7pdxydbGF2X4TxVusJm"),
+    inpNanos: 9_000_000,
+    outNanos: 10_000_000,
+  },
+];
+
+const EXPECTED_ACC_LEN = ENTRIES.length * ENTRY_LEN + 32;
+
+assert(EXPECTED_ACC_LEN === 232, `Actual: ${EXPECTED_ACC_LEN}`);
+
+function entryBytes({ mint, inpNanos, outNanos }: Entry): Uint8Array {
+  const bytes = new Uint8Array(ENTRY_LEN);
+  bytes.set(getAddressEncoder().encode(mint), 0);
+  bytes.set(getI32Encoder().encode(inpNanos), 32);
+  bytes.set(getI32Encoder().encode(outNanos), 36);
+  return bytes;
+}
+
+function main() {
+  const entries = ENTRIES.map(entryBytes).sort((a, b) => {
+    const apk = getAddressDecoder().decode(a);
+    const bpk = getAddressDecoder().decode(b);
+    return getAddressComparator()(apk, bpk);
+  });
+
+  // admin is just all zeros
+  const data = new Uint8Array([
+    ...getAddressEncoder().encode(address("11111111111111111111111111111111")),
+    ...entries.flatMap((a) => [...a]),
+  ]);
+
+  assert(data.length === EXPECTED_ACC_LEN, `Actual: ${data.length}`);
+
+  // NB: order of the fields here affect order of the fields in json
+  writeTestFixturesAcc(FLATSLAB_SLAB_NAME, {
+    pubkey: address("4T9YzXnmQFMyYi2nrxyXjhtUANavmCkxGCsU3GKaNjwT"),
+    account: {
+      lamports: lamports(2_505_600n), // solana rent 232
+      data: [getBase64Decoder().decode(data) as Base64EncodedBytes, "base64"],
+      owner: address("s1b6NRXj6ygNu1QMKXh2H9LUR2aPApAAm1UQ2DjdhNV"),
+      executable: false,
+      rentEpoch: 18446744073709551615n,
+      space: BigInt(EXPECTED_ACC_LEN),
+    },
+  });
+}
+
+main();
