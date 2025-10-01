831460e0ac81f1e5012ef4615e12a50bbb32fdf8
billythedummy <dhy1996@live.com.sg>
2025-10-01 13:44:46 +0800

    add snapshot test

--- controller/program/tests/tests/sync_sol_value.rs
+++ controller/program/tests/tests/sync_sol_value.rs
@@ -1,8 +1,10 @@
+use expect_test::{expect, Expect};
 use inf1_core::instructions::sync_sol_value::{
     sync_sol_value_ix_is_signer, sync_sol_value_ix_is_writer, sync_sol_value_ix_keys_owned,
     SyncSolValueIxAccs,
 };
 use inf1_ctl_jiminy::{
+    accounts::{lst_state_list::LstStatePackedList, pool_state::PoolStatePacked},
     instructions::sync_sol_value::{
         NewSyncSolValueIxPreAccsBuilder, SyncSolValueIxData, SyncSolValueIxPreKeysOwned,
     },
@@ -13,7 +15,8 @@ use inf1_svc_ag_core::{
     inf1_svc_lido_core::solido_legacy_core::TOKENKEG_PROGRAM, instructions::SvcCalcAccsAg, SvcAgTy,
 };
 use inf1_test_utils::{
-    keys_signer_writable_to_metas, PkAccountTup, ALL_FIXTURES, JUPSOL_FIXTURE_LST_IDX, JUPSOL_MINT,
+    acc_bef_aft, keys_signer_writable_to_metas, PkAccountTup, ALL_FIXTURES, JUPSOL_FIXTURE_LST_IDX,
+    JUPSOL_MINT,
 };
 use mollusk_svm::result::{InstructionResult, ProgramResult};
 use solana_instruction::Instruction;
@@ -60,6 +63,47 @@ fn sync_sol_value_fixtures_accounts(builder: &SyncSolValueKeysBuilder) -> Vec<Pk
         .collect()
 }
 
+fn assert_correct_sync(bef: &[PkAccountTup], aft: &[PkAccountTup], mint: &[u8; 32]) -> i128 {
+    let [pools, lst_state_lists] = [POOL_STATE_ID, LST_STATE_LIST_ID]
+        .map(|a| acc_bef_aft(&Pubkey::new_from_array(a), bef, aft));
+    let [pool_bef, pool_aft] = pools.each_ref().map(|a| {
+        PoolStatePacked::of_acc_data(&a.data)
+            .unwrap()
+            .into_pool_state()
+    });
+    let [lst_state_list_bef, lst_state_list_aft] = lst_state_lists
+        .each_ref()
+        .map(|a| LstStatePackedList::of_acc_data(&a.data).unwrap());
+    let lst_state_i = lst_state_list_bef
+        .0
+        .iter()
+        .position(|s| s.into_lst_state().mint == *mint)
+        .unwrap();
+    let [lst_state_bef, lst_state_aft] =
+        [lst_state_list_bef, lst_state_list_aft].map(|l| l.0[lst_state_i].into_lst_state());
+
+    assert_eq!(lst_state_bef.mint, *mint);
+    assert_eq!(lst_state_bef.mint, lst_state_aft.mint);
+
+    let delta = i128::from(pool_aft.total_sol_value) - i128::from(pool_bef.total_sol_value);
+    assert_eq!(
+        delta,
+        i128::from(lst_state_aft.sol_value) - i128::from(lst_state_bef.sol_value)
+    );
+
+    delta
+}
+
+fn assert_correct_sync_snapshot(
+    bef: &[PkAccountTup],
+    aft: &[PkAccountTup],
+    mint: &[u8; 32],
+    expected_sol_val_delta: Expect,
+) {
+    let delta = assert_correct_sync(bef, aft, mint);
+    expected_sol_val_delta.assert_eq(&delta.to_string());
+}
+
 #[test]
 fn sync_sol_value_jupsol_fixture() {
     let ix_prefix = sync_sol_value_ix_pre_keys_owned(&TOKENKEG_PROGRAM, JUPSOL_MINT.to_bytes());
@@ -71,11 +115,21 @@ fn sync_sol_value_jupsol_fixture() {
     let ix = sync_sol_value_ix(&builder, JUPSOL_FIXTURE_LST_IDX as u32);
     let accounts = sync_sol_value_fixtures_accounts(&builder);
 
-    let InstructionResult { program_result, .. } =
-        SVM.with(|svm| svm.process_instruction(&ix, &accounts));
+    let InstructionResult {
+        program_result,
+        resulting_accounts,
+        ..
+    } = SVM.with(|svm| svm.process_instruction(&ix, &accounts));
 
     assert!(
         matches!(program_result, ProgramResult::Success),
         "{program_result:#?}"
+    );
+
+    assert_correct_sync_snapshot(
+        &accounts,
+        &resulting_accounts,
+        JUPSOL_MINT.as_array(),
+        expect!["547883064440"],
     );
 }
