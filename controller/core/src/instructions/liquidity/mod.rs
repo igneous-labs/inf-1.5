0086f5b34f6e2ed7c7944468177bbbea2999b55f
lifeofpavs <adell13pablo@gmail.com>
2025-10-16 09:57:20 +0200

    revert nit

--- controller/core/src/instructions/liquidity/mod.rs
+++ controller/core/src/instructions/liquidity/mod.rs
@@ -73,7 +73,6 @@ pub struct IxArgs {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-//Need to define here the parsing function for all the data.
 pub struct IxData<const DISCM: u8>([u8; IX_DATA_LEN]);
 
 impl<const DISCM: u8> IxData<DISCM> {
