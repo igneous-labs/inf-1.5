37446ccb8284acc54790975d8ceefff2f51f2559
lifeofpavs <adell13pablo@gmail.com>
2025-11-04 14:52:09 +0100

    feta: pdating

--- test-utils/src/accounts/system.rs
+++ test-utils/src/accounts/system.rs
@@ -3,6 +3,7 @@ use solana_account::Account;
 pub fn mock_system_acc(lamports: u64) -> Account {
     Account {
         lamports,
+        rent_epoch: u64::MAX,
         ..Default::default()
     }
 }
