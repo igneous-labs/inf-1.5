4cb389151e67c0b700a0f2f53711d0d42ae6bf21
billythedummy <dhy1996@live.com.sg>
2025-11-17 12:26:02 +0800

    container logs

--- ts/tests/scripts/run-test.sh
+++ ts/tests/scripts/run-test.sh
@@ -5,8 +5,14 @@ DOCKER_COMPOSE_LOCAL_VALIDATOR="$REPO_DIR/docker-compose-local-validator.yml"
 
 # Ensure cleanup happens on script exit
 cleanup() {
+    echo "validator logs:"
+    echo ""
+    docker compose -f $DOCKER_COMPOSE_LOCAL_VALIDATOR logs
+
+    echo ""
+
     # Clean up solana-test-validator container
-    echo "Cleaning up containers..."
+    echo "Cleaning up validator..."
     docker compose -f $DOCKER_COMPOSE_LOCAL_VALIDATOR down -v
 }
 
