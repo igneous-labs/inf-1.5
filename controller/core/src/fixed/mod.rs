2ba2f65f2f7e0a609b77b667f9c5de7ebb7c23f7
billythedummy <dhy1996@live.com.sg>
2025-11-24 13:39:48 +0800

    exp

--- controller/core/src/fixed/mod.rs
+++ controller/core/src/fixed/mod.rs
@@ -13,37 +13,70 @@ use core::ops::Mul;
 use sanctum_u64_ratio::Ratio;
 
 #[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct UQ0_64(pub u64);
 
 #[inline]
-pub const fn uq0_64_floor_mul(UQ0_64(a): UQ0_64, UQ0_64(b): UQ0_64) -> UQ0_64 {
+pub const fn uq0_64_mul(UQ0_64(a): UQ0_64, UQ0_64(b): UQ0_64) -> UQ0_64 {
+    const ROUNDING_BIAS: u128 = 1 << 63;
+
     // where d = u64::MAX
     // (n1/d) * (n2/d) = n1*n2/d^2
     // as-safety: u128 bitwidth > u64 bitwidth
     // unchecked arith safety: u64*u64 never overflows u128
     let res = (a as u128) * (b as u128);
+    // round off 64th bit
+    // unchecked arith safety: res <= u64::MAX * u64::MAX + ROUNDING_BIAS < u128::MAX
+    let rounded = res + ROUNDING_BIAS;
     // convert back to UQ0_64 by making denom = d
     // n1*n2/d^2 = (n1*n2/d) / d
     // so we need to divide res by d,
     // and division by u64::MAX is just >> 64
     // as-safety: truncating conversion is what we want
     // to achieve floor mul
-    UQ0_64((res >> 64) as u64)
+    UQ0_64((rounded >> 64) as u64)
+}
+
+#[inline]
+pub const fn uq0_64_into_ratio(a: UQ0_64) -> Ratio<u64, u64> {
+    Ratio {
+        n: a.0,
+        d: u64::MAX,
+    }
+}
+
+#[inline]
+pub const fn uq0_64_pow(mut base: UQ0_64, mut exp: u64) -> UQ0_64 {
+    // sq & mul
+    let mut res = UQ0_64::ONE;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            res = uq0_64_mul(res, base);
+        }
+        base = uq0_64_mul(base, base);
+        exp /= 2;
+    }
+    res
 }
 
 impl UQ0_64 {
+    pub const ZERO: Self = Self(0);
+    pub const ONE: Self = Self(u64::MAX);
+
+    /// Rounding is to closest bit
     #[inline]
-    pub const fn floor_mul(a: Self, b: Self) -> Self {
-        uq0_64_floor_mul(a, b)
+    pub const fn const_mul(a: Self, b: Self) -> Self {
+        uq0_64_mul(a, b)
+    }
+
+    #[inline]
+    pub const fn pow(self, exp: u64) -> Self {
+        uq0_64_pow(self, exp)
     }
 
     #[inline]
     pub const fn into_ratio(self) -> Ratio<u64, u64> {
-        Ratio {
-            n: self.0,
-            d: u64::MAX,
-        }
+        uq0_64_into_ratio(self)
     }
 }
 
@@ -53,7 +86,7 @@ impl Mul for UQ0_64 {
     /// Rounding floors
     #[inline]
     fn mul(self, rhs: Self) -> Self::Output {
-        Self::floor_mul(self, rhs)
+        Self::const_mul(self, rhs)
     }
 }
 
@@ -68,12 +101,23 @@ impl From<UQ0_64> for Ratio<u64, u64> {
 mod tests {
     use core::cmp::min;
 
+    use expect_test::expect;
     use proptest::prelude::*;
     use sanctum_u64_ratio::Ratio;
 
     use super::*;
 
     const D: f64 = u64::MAX as f64;
+
+    /// max error bounds for multiplication
+    /// - UQ0_64. 1-bit, so 2^-64
+    /// - f64 for range 0.0-1.0, around 2^-54 (around 2^10 larger than UQ0_64 because fewer bits dedicated to fraction)
+    const MAX_MUL_DIFF_F64_VS_US: u64 = 4096;
+
+    const EPSILON_RATIO_DIFF: Ratio<u64, u64> = Ratio {
+        n: 1,
+        d: 1_000_000_000_000,
+    };
 
     const fn f64_approx(UQ0_64(a): UQ0_64) -> f64 {
         (a as f64) / D
@@ -95,22 +139,64 @@ mod tests {
             let approx_f64 = [a, b].map(f64_approx).into_iter().reduce(core::ops::Mul::mul).unwrap();
             let approx_uq0_64 = uq0_64_approx(approx_f64);
 
-            // max error bounds for multiplication
-            // - UQ0_64. 1-bit, so 2^-64
-            // - f64 for range 0.0-1.0, around 2^-54 (around 2^10 larger than UQ0_64 because fewer bits dedicated to fraction)
+            // small error from f64 result
             let diff_u64 = us.0.abs_diff(approx_uq0_64.0);
             prop_assert!(
-                diff_u64 <= 4096,
+                diff_u64 <= MAX_MUL_DIFF_F64_VS_US,
                 "{}, {}",
                 us.0,
                 approx_uq0_64.0
             );
-            // diff should not exceed 1 / 1_000_000_000 of value
+
+            // diff should not exceed epsilon proportion of value
             let diff_r = Ratio {
                 n: diff_u64,
                 d: min(us.0, approx_uq0_64.0),
             };
-            prop_assert!(diff_r < Ratio { n: 1, d: 1_000_000_000 }, "diff_r: {diff_r}");
+            prop_assert!(diff_r < EPSILON_RATIO_DIFF, "diff_r: {diff_r}");
         }
+    }
+
+    proptest! {
+        #[test]
+        fn exp_pt(base: u64, exp: u64) {
+            let base = UQ0_64(base);
+            let us = base.pow(exp);
+
+            let approx_f64 = f64_approx(us).powf(exp as f64);
+            let approx_uq0_64 = uq0_64_approx(approx_f64);
+
+            // small error from f64 result
+            let diff_u64 = us.0.abs_diff(approx_uq0_64.0);
+            // same err bound as mul_pt since a.pow(2) = a * a
+            prop_assert!(
+                diff_u64 <= MAX_MUL_DIFF_F64_VS_US,
+                "{}, {}",
+                us.0,
+                approx_uq0_64.0
+            );
+
+            // diff should not exceed epsilon proportion of value
+            let diff_r = Ratio {
+                n: diff_u64,
+                d: min(us.0, approx_uq0_64.0),
+            };
+            prop_assert!(diff_r < EPSILON_RATIO_DIFF, "diff_r: {diff_r}");
+
+            // compare against naive multiplication implementation
+            const LIM: u64 = u16::MAX as u64;
+            let naive_mul_res = match exp {
+                0 => UQ0_64::ONE,
+                1..=LIM => (0..exp).fold(base, |res, _| res * base),
+                _will_take_too_long_to_run => return Ok(())
+            };
+            prop_assert_eq!(naive_mul_res, us);
+        }
+    }
+
+    #[test]
+    fn into_ratio_sc() {
+        expect!["9223372036854775807/18446744073709551615"]
+            .assert_eq(&UQ0_64(u64::MAX / 2).into_ratio().to_string());
     }
 }
