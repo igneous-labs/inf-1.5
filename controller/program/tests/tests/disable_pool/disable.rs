7751fdfaa2a00dba35fe9c084aed9b4033815173
billythedummy <dhy1996@live.com.sg>
2025-11-26 21:52:37 +0800

    fix disable pool tests

--- controller/program/tests/tests/disable_pool/disable.rs
+++ controller/program/tests/tests/disable_pool/disable.rs
@@ -1,5 +1,7 @@
 use inf1_ctl_jiminy::{
-    accounts::pool_state::{PoolState, PoolStatePacked},
+    accounts::pool_state::{
+        PoolStateV2, PoolStateV2Addrs, PoolStateV2FtaVals, PoolStateV2Packed, PoolStateV2U8Bools,
+    },
     err::Inf1CtlErr,
     instructions::disable_pool::disable::{
         DisablePoolIxData, DisablePoolIxKeysOwned, NewDisablePoolIxAccsBuilder,
@@ -10,15 +12,16 @@ use inf1_ctl_jiminy::{
     ID,
 };
 use inf1_test_utils::{
-    acc_bef_aft, any_disable_pool_auth_list, any_pool_state, assert_diffs_pool_state,
-    assert_jiminy_prog_err, disable_pool_auth_list_account, gen_pool_state,
-    keys_signer_writable_to_metas, list_sample_flat_map, mock_sys_acc, mollusk_exec,
-    pool_state_account, silence_mollusk_logs, AccountMap, AnyPoolStateArgs, Diff,
-    DiffsPoolStateArgs, GenPoolStateArgs, PoolStateBools, PoolStatePks,
+    acc_bef_aft, any_disable_pool_auth_list, any_pool_state_v2, assert_diffs_pool_state_v2,
+    assert_jiminy_prog_err, disable_pool_auth_list_account, keys_signer_writable_to_metas,
+    list_sample_flat_map, mock_sys_acc, mollusk_exec, pool_state_v2_account,
+    pool_state_v2_u8_bools_normal_strat, silence_mollusk_logs, AccountMap, Diff, DiffsPoolStateV2,
+    PoolStateV2FtaStrat,
 };
 use jiminy_cpi::program_error::{ProgramError, MISSING_REQUIRED_SIGNATURE};
 use mollusk_svm::result::{InstructionResult, ProgramResult};
 use proptest::{prelude::*, strategy::Union};
+use solana_account::Account;
 use solana_instruction::Instruction;
 use solana_pubkey::Pubkey;
 
@@ -37,9 +40,9 @@ fn disable_pool_ix(keys: DisablePoolIxKeysOwned) -> Instruction {
     }
 }
 
-fn disable_pool_test_accs(
+fn test_accs(
     keys: DisablePoolIxKeysOwned,
-    pool: PoolState,
+    pool: Account,
     // disable pool authority list
     dpal: Vec<[u8; 32]>,
 ) -> AccountMap {
@@ -48,9 +51,18 @@ fn disable_pool_test_accs(
     let accs = NewDisablePoolIxAccsBuilder::start()
         .with_signer(mock_sys_acc(LAMPORTS))
         .with_disable_pool_auth_list(disable_pool_auth_list_account(dpal))
-        .with_pool_state(pool_state_account(pool))
+        .with_pool_state(pool)
         .build();
     keys.0.into_iter().map(Into::into).zip(accs.0).collect()
+}
+
+fn disable_pool_test_accs(
+    keys: DisablePoolIxKeysOwned,
+    pool: PoolStateV2,
+    // disable pool authority list
+    dpal: Vec<[u8; 32]>,
+) -> AccountMap {
+    test_accs(keys, pool_state_v2_account(pool), dpal)
 }
 
 fn disable_pool_test(
@@ -70,17 +82,17 @@ fn disable_pool_test(
     let aft: AccountMap = resulting_accounts.into_iter().collect();
     let [pool_state_bef, pool_state_aft] =
         acc_bef_aft(&POOL_STATE_ID.into(), &bef, &aft).map(|a| {
-            PoolStatePacked::of_acc_data(&a.data)
+            PoolStateV2Packed::of_acc_data(&a.data)
                 .unwrap()
-                .into_pool_state()
+                .into_pool_state_v2()
         });
 
     match expected_err {
         None => {
             assert_eq!(program_result, ProgramResult::Success);
-            assert_diffs_pool_state(
-                &DiffsPoolStateArgs {
-                    bools: PoolStateBools::default()
+            assert_diffs_pool_state_v2(
+                &DiffsPoolStateV2 {
+                    u8_bools: PoolStateV2U8Bools::default()
                         // strict because ix is not supposed to succeed if pool already disabled
                         .with_is_disabled(Diff::StrictChanged(false, true)),
                     ..Default::default()
@@ -98,11 +110,11 @@ fn disable_pool_test(
 #[test]
 fn disable_pool_test_correct_basic() {
     let admin = [69u8; 32];
-    let pool = gen_pool_state(GenPoolStateArgs {
-        pks: PoolStatePks::default().with_admin(admin),
-        version: 1,
+    let pool = PoolStateV2FtaVals {
+        addrs: PoolStateV2Addrs::default().with_admin(admin),
         ..Default::default()
-    });
+    }
+    .into_pool_state_v2();
     let keys = NewDisablePoolIxAccsBuilder::start()
         .with_signer(admin)
         .with_disable_pool_auth_list(DISABLE_POOL_AUTHORITY_LIST_ID)
@@ -124,15 +136,15 @@ fn correct_keys(signer: [u8; 32]) -> DisablePoolIxKeysOwned {
 }
 
 fn to_inp(
-    (k, ps, dpal): (DisablePoolIxKeysOwned, PoolState, Vec<[u8; 32]>),
+    (k, ps, dpal): (DisablePoolIxKeysOwned, PoolStateV2, Vec<[u8; 32]>),
 ) -> (Instruction, AccountMap) {
     (disable_pool_ix(k), disable_pool_test_accs(k, ps, dpal))
 }
 
 fn correct_admin_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
     (
-        any_pool_state(AnyPoolStateArgs {
-            bools: PoolStateBools::normal(),
+        any_pool_state_v2(PoolStateV2FtaStrat {
+            u8_bools: pool_state_v2_u8_bools_normal_strat(),
             ..Default::default()
         }),
         any_disable_pool_auth_list(0..=MAX_DISABLE_POOL_AUTH_LIST_LEN),
@@ -156,8 +168,8 @@ fn correct_disable_auth_strat() -> impl Strategy<Value = (Instruction, AccountMa
         .prop_flat_map(|l| {
             (
                 list_sample_flat_map(l),
-                any_pool_state(AnyPoolStateArgs {
-                    bools: PoolStateBools::normal(),
+                any_pool_state_v2(PoolStateV2FtaStrat {
+                    u8_bools: pool_state_v2_u8_bools_normal_strat(),
                     ..Default::default()
                 }),
             )
@@ -179,8 +191,8 @@ proptest! {
 fn unauthorized_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
     (
         any_disable_pool_auth_list(0..=MAX_DISABLE_POOL_AUTH_LIST_LEN),
-        any_pool_state(AnyPoolStateArgs {
-            bools: PoolStateBools::normal(),
+        any_pool_state_v2(PoolStateV2FtaStrat {
+            u8_bools: pool_state_v2_u8_bools_normal_strat(),
             ..Default::default()
         }),
     )
@@ -239,8 +251,9 @@ fn rebalancing_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
     any_disable_pool_auth_list(1..=MAX_DISABLE_POOL_AUTH_LIST_LEN)
         .prop_flat_map(|dpal| {
             (
-                any_pool_state(AnyPoolStateArgs {
-                    bools: PoolStateBools::normal().with_is_rebalancing(Some(Just(true).boxed())),
+                any_pool_state_v2(PoolStateV2FtaStrat {
+                    u8_bools: pool_state_v2_u8_bools_normal_strat()
+                        .with_is_rebalancing(Some(Just(true).boxed())),
                     ..Default::default()
                 }),
                 list_sample_flat_map(dpal),
@@ -275,8 +288,9 @@ fn disabled_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
     any_disable_pool_auth_list(1..=MAX_DISABLE_POOL_AUTH_LIST_LEN)
         .prop_flat_map(|dpal| {
             (
-                any_pool_state(AnyPoolStateArgs {
-                    bools: PoolStateBools::normal().with_is_disabled(Some(Just(true).boxed())),
+                any_pool_state_v2(PoolStateV2FtaStrat {
+                    u8_bools: pool_state_v2_u8_bools_normal_strat()
+                        .with_is_disabled(Some(Just(true).boxed())),
                     ..Default::default()
                 }),
                 list_sample_flat_map(dpal),
