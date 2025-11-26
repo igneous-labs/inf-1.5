b420926072c4e27adeb7779a471ef1ce7ae2a308
billythedummy <dhy1996@live.com.sg>
2025-11-26 22:35:08 +0800

    update enable pool tests

--- controller/program/tests/tests/disable_pool/enable.rs
+++ controller/program/tests/tests/disable_pool/enable.rs
@@ -1,5 +1,7 @@
 use inf1_ctl_jiminy::{
-    accounts::pool_state::{PoolState, PoolStatePacked},
+    accounts::pool_state::{
+        PoolStateV2, PoolStateV2Addrs, PoolStateV2FtaVals, PoolStateV2Packed, PoolStateV2U8Bools,
+    },
     err::Inf1CtlErr,
     instructions::disable_pool::enable::{
         EnablePoolIxData, EnablePoolIxKeysOwned, NewEnablePoolIxAccsBuilder,
@@ -10,10 +12,10 @@ use inf1_ctl_jiminy::{
     ID,
 };
 use inf1_test_utils::{
-    acc_bef_aft, any_pool_state, assert_diffs_pool_state, assert_jiminy_prog_err, gen_pool_state,
-    keys_signer_writable_to_metas, mock_sys_acc, mollusk_exec, pool_state_account,
-    silence_mollusk_logs, AccountMap, AnyPoolStateArgs, Diff, DiffsPoolStateArgs, GenPoolStateArgs,
-    PoolStateBools, PoolStatePks,
+    acc_bef_aft, any_pool_state_v2, assert_diffs_pool_state_v2, assert_jiminy_prog_err, bool_to_u8,
+    keys_signer_writable_to_metas, mock_sys_acc, mollusk_exec, pool_state_v2_account,
+    pool_state_v2_u8_bools_normal_strat, silence_mollusk_logs, AccountMap, Diff, DiffsPoolStateV2,
+    PoolStateV2FtaStrat,
 };
 use jiminy_cpi::program_error::{ProgramError, INVALID_ARGUMENT, MISSING_REQUIRED_SIGNATURE};
 use mollusk_svm::result::{InstructionResult, ProgramResult};
@@ -36,12 +38,12 @@ fn enable_pool_ix(keys: EnablePoolIxKeysOwned) -> Instruction {
     }
 }
 
-fn enable_pool_test_accs(keys: EnablePoolIxKeysOwned, pool: PoolState) -> AccountMap {
+fn enable_pool_test_accs(keys: EnablePoolIxKeysOwned, pool: PoolStateV2) -> AccountMap {
     // dont care abt lamports, shouldnt affect anything
     const LAMPORTS: u64 = 1_000_000_000;
     let accs = NewEnablePoolIxAccsBuilder::start()
         .with_admin(mock_sys_acc(LAMPORTS))
-        .with_pool_state(pool_state_account(pool))
+        .with_pool_state(pool_state_v2_account(pool))
         .build();
     keys.0.into_iter().map(Into::into).zip(accs.0).collect()
 }
@@ -63,17 +65,17 @@ fn enable_pool_test(
     let aft: AccountMap = resulting_accounts.into_iter().collect();
     let [pool_state_bef, pool_state_aft] =
         acc_bef_aft(&POOL_STATE_ID.into(), &bef, &aft).map(|a| {
-            PoolStatePacked::of_acc_data(&a.data)
+            PoolStateV2Packed::of_acc_data(&a.data)
                 .unwrap()
-                .into_pool_state()
+                .into_pool_state_v2()
         });
 
     match expected_err {
         None => {
             assert_eq!(program_result, ProgramResult::Success);
-            assert_diffs_pool_state(
-                &DiffsPoolStateArgs {
-                    bools: PoolStateBools::default()
+            assert_diffs_pool_state_v2(
+                &DiffsPoolStateV2 {
+                    u8_bools: PoolStateV2U8Bools::default()
                         // strict because ix is not supposed to succeed if pool already enabled
                         .with_is_disabled(Diff::StrictChanged(true, false)),
                     ..Default::default()
@@ -91,12 +93,12 @@ fn enable_pool_test(
 #[test]
 fn enable_pool_test_correct_basic() {
     let admin = [69u8; 32];
-    let pool = gen_pool_state(GenPoolStateArgs {
-        pks: PoolStatePks::default().with_admin(admin),
-        bools: PoolStateBools::default().with_is_disabled(true),
-        version: 1,
+    let pool = PoolStateV2FtaVals {
+        addrs: PoolStateV2Addrs::default().with_admin(admin),
+        u8_bools: PoolStateV2U8Bools::default().with_is_disabled(bool_to_u8(true)),
         ..Default::default()
-    });
+    }
+    .into_pool_state_v2();
     let keys = NewEnablePoolIxAccsBuilder::start()
         .with_admin(admin)
         .with_pool_state(POOL_STATE_ID)
@@ -115,13 +117,13 @@ fn correct_keys(admin: [u8; 32]) -> EnablePoolIxKeysOwned {
         .build()
 }
 
-fn to_inp((k, ps): (EnablePoolIxKeysOwned, PoolState)) -> (Instruction, AccountMap) {
+fn to_inp((k, ps): (EnablePoolIxKeysOwned, PoolStateV2)) -> (Instruction, AccountMap) {
     (enable_pool_ix(k), enable_pool_test_accs(k, ps))
 }
 
 fn correct_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
-    any_pool_state(AnyPoolStateArgs {
-        bools: PoolStateBools::normal().with_is_disabled(Some(Just(true).boxed())),
+    any_pool_state_v2(PoolStateV2FtaStrat {
+        u8_bools: pool_state_v2_u8_bools_normal_strat().with_is_disabled(Some(Just(true).boxed())),
         ..Default::default()
     })
     .prop_map(|ps| (correct_keys(ps.admin), ps))
@@ -139,8 +141,8 @@ proptest! {
 }
 
 fn unauthorized_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
-    any_pool_state(AnyPoolStateArgs {
-        bools: PoolStateBools::normal().with_is_disabled(Some(Just(true).boxed())),
+    any_pool_state_v2(PoolStateV2FtaStrat {
+        u8_bools: pool_state_v2_u8_bools_normal_strat().with_is_disabled(Some(Just(true).boxed())),
         ..Default::default()
     })
     .prop_flat_map(|ps| {
@@ -185,8 +187,8 @@ proptest! {
 }
 
 fn enabled_strat() -> impl Strategy<Value = (Instruction, AccountMap)> {
-    any_pool_state(AnyPoolStateArgs {
-        bools: PoolStateBools::normal().with_is_disabled(Some(Just(false).boxed())),
+    any_pool_state_v2(PoolStateV2FtaStrat {
+        u8_bools: pool_state_v2_u8_bools_normal_strat(),
         ..Default::default()
     })
     .prop_map(|ps| (correct_keys(ps.admin), ps))
