437d32b005f1723d98988db9db3ff52d859b8ba7
billythedummy <dhy1996@live.com.sg>
2025-12-05 17:17:23 +0800

    mollusk_with_clock_override

--- controller/program/tests/common/mollusk.rs
+++ controller/program/tests/common/mollusk.rs
@@ -1,6 +1,10 @@
+use std::cell::RefCell;
+
 use inf1_test_utils::mollusk_inf_local_ctl;
 use mollusk_svm::Mollusk;
 
 thread_local! {
-    pub static SVM: Mollusk = mollusk_inf_local_ctl()
+    pub static SVM: Mollusk = mollusk_inf_local_ctl();
+
+    pub static SVM_MUT: RefCell<Mollusk> = RefCell::new(mollusk_inf_local_ctl());
 }
