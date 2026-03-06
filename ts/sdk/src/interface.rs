035daf28201bc08e20a388338a56b71ad2deb3e9
billythedummy <dhy1996@live.com.sg>
2026-03-06 23:02:24 +0800

    impl

--- ts/sdk/src/interface.rs
+++ ts/sdk/src/interface.rs
@@ -58,7 +58,7 @@ pub struct Account {
     pub owner: B58PK,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Tsify)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi, large_number_types_as_bigints)]
 #[serde(rename_all = "camelCase")]
 pub struct PoolStateV2 {
@@ -79,7 +79,85 @@ pub struct PoolStateV2 {
     pub last_release_slot: u64,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Tsify)]
+pub const fn pool_state_v2_from_intf(
+    PoolStateV2 {
+        total_sol_value,
+        protocol_fee_nanos,
+        version,
+        is_disabled,
+        is_rebalancing,
+        admin: Bs58Array(admin),
+        rebalance_authority: Bs58Array(rebalance_authority),
+        protocol_fee_beneficiary: Bs58Array(protocol_fee_beneficiary),
+        pricing_program: Bs58Array(pricing_program),
+        lp_token_mint: Bs58Array(lp_token_mint),
+        rps_authority: Bs58Array(rps_authority),
+        rps,
+        withheld_lamports,
+        protocol_fee_lamports,
+        last_release_slot,
+    }: PoolStateV2,
+) -> inf1_std::inf1_ctl_core::accounts::pool_state::PoolStateV2 {
+    inf1_std::inf1_ctl_core::accounts::pool_state::PoolStateV2 {
+        total_sol_value,
+        protocol_fee_nanos,
+        version,
+        is_disabled,
+        is_rebalancing,
+        padding: [0],
+        rps,
+        withheld_lamports,
+        protocol_fee_lamports,
+        last_release_slot,
+        admin,
+        rebalance_authority,
+        protocol_fee_beneficiary,
+        pricing_program,
+        lp_token_mint,
+        rps_authority,
+    }
+}
+
+pub const fn pool_state_v2_into_intf(
+    inf1_std::inf1_ctl_core::accounts::pool_state::PoolStateV2 {
+        total_sol_value,
+        protocol_fee_nanos,
+        version,
+        is_disabled,
+        is_rebalancing,
+        rps,
+        withheld_lamports,
+        protocol_fee_lamports,
+        last_release_slot,
+        admin,
+        rebalance_authority,
+        protocol_fee_beneficiary,
+        pricing_program,
+        lp_token_mint,
+        rps_authority,
+        padding: _,
+    }: inf1_std::inf1_ctl_core::accounts::pool_state::PoolStateV2,
+) -> PoolStateV2 {
+    PoolStateV2 {
+        total_sol_value,
+        admin: B58PK::new(admin),
+        is_disabled,
+        is_rebalancing,
+        lp_token_mint: B58PK::new(lp_token_mint),
+        pricing_program: B58PK::new(pricing_program),
+        protocol_fee_beneficiary: B58PK::new(protocol_fee_beneficiary),
+        rebalance_authority: B58PK::new(rebalance_authority),
+        version,
+        protocol_fee_nanos,
+        rps_authority: B58PK::new(rps_authority),
+        rps,
+        withheld_lamports,
+        protocol_fee_lamports,
+        last_release_slot,
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi, large_number_types_as_bigints)]
 #[serde(rename_all = "camelCase")]
 pub struct LstState {
@@ -89,4 +167,46 @@ pub struct LstState {
     pub sol_value: u64,
     pub mint: B58PK,
     pub sol_value_calculator: B58PK,
+}
+
+pub const fn lst_state_from_intf(
+    LstState {
+        is_input_disabled,
+        mint: Bs58Array(mint),
+        pool_reserves_bump,
+        protocol_fee_accumulator_bump,
+        sol_value,
+        sol_value_calculator: Bs58Array(sol_value_calculator),
+    }: LstState,
+) -> inf1_std::inf1_ctl_core::typedefs::lst_state::LstState {
+    inf1_std::inf1_ctl_core::typedefs::lst_state::LstState {
+        is_input_disabled,
+        mint,
+        pool_reserves_bump,
+        protocol_fee_accumulator_bump,
+        sol_value,
+        sol_value_calculator,
+        padding: [0; 5],
+    }
+}
+
+pub const fn lst_state_into_intf(
+    inf1_std::inf1_ctl_core::typedefs::lst_state::LstState {
+        is_input_disabled,
+        mint,
+        pool_reserves_bump,
+        protocol_fee_accumulator_bump,
+        sol_value,
+        sol_value_calculator,
+        padding: _,
+    }: inf1_std::inf1_ctl_core::typedefs::lst_state::LstState,
+) -> LstState {
+    LstState {
+        is_input_disabled,
+        mint: B58PK::new(mint),
+        pool_reserves_bump,
+        protocol_fee_accumulator_bump,
+        sol_value,
+        sol_value_calculator: B58PK::new(sol_value_calculator),
+    }
 }
