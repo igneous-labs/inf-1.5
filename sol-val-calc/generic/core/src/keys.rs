467630a6c2e14220a07b9a9b6e64f36b4761abfd
billythedummy <dhy1996@live.com.sg>
2026-07-06 19:29:52 +0800

    const pda derive and expect-test

--- sol-val-calc/generic/core/src/keys.rs
+++ sol-val-calc/generic/core/src/keys.rs
@@ -1,40 +1,5 @@
 use generic_array_struct::generic_array_struct;
 
-/// Program-specific const addrs
-#[generic_array_struct(all pub)]
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ConstAccs<T> {
-    /// This program's (generic SOL Value Calculator) program ID
-    pub program: T,
-
-    /// The stake pool program that this SVC program works with
-    pub pool_prog: T,
-
-    /// `pool_prog`'s BPF loader V3 program data PDA account
-    pub pool_prog_data: T,
-
-    /// initial manager to set to
-    pub init_manager: T,
-}
-
-#[generic_array_struct(all pub)]
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct GlobalConstAccs<T> {
-    pub sys_prog: T,
-}
-
-pub const GLOBAL_CONST_KEY_STRS: GlobalConstAccs<&str> =
-    GlobalConstAccs::const_from_destr(GlobalConstAccsDestr {
-        sys_prog: "11111111111111111111111111111111",
-    });
-
-pub const GLOBAL_CONST_KEYS_OWNED: GlobalConstAccs<[u8; 32]> = GlobalConstAccs(crate::const_map!(
-    [0; 32],
-    GLOBAL_CONST_KEY_STRS.0,
-    const_crypto::bs58::decode_pubkey
-));
-
-#[macro_export]
 macro_rules! const_map {
     ($DEFAULT:expr, $from_arr:expr, $const_fn:expr) => {{
         let mut res = [$DEFAULT; _];
@@ -46,3 +11,48 @@ macro_rules! const_map {
         res
     }};
 }
+pub(crate) use const_map;
+
+/// Program-specific const addrs
+#[generic_array_struct(all pub)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ConstAccs<T> {
+    /// This program's (generic SOL Value Calculator) program ID
+    pub program: T,
+
+    /// The stake pool program that this SVC program works with
+    pub pool_prog: T,
+
+    /// initial manager to set to
+    pub init_manager: T,
+}
+
+impl ConstAccs<&str> {
+    pub const fn const_keys(self) -> ConstAccs<[u8; 32]> {
+        ConstAccs(const_map!(
+            [0; 32],
+            self.0,
+            const_crypto::bs58::decode_pubkey
+        ))
+    }
+}
+
+#[generic_array_struct(all pub)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct GlobalConstAccs<T> {
+    pub sys_prog: T,
+
+    pub bpf_loader_v3: T,
+}
+
+pub const GLOBAL_CONST_KEY_STRS: GlobalConstAccs<&str> =
+    GlobalConstAccs::const_from_destr(GlobalConstAccsDestr {
+        sys_prog: "11111111111111111111111111111111",
+        bpf_loader_v3: "BPFLoaderUpgradeab1e11111111111111111111111",
+    });
+
+pub const GLOBAL_CONST_KEYS_OWNED: GlobalConstAccs<[u8; 32]> = GlobalConstAccs(const_map!(
+    [0; 32],
+    GLOBAL_CONST_KEY_STRS.0,
+    const_crypto::bs58::decode_pubkey
+));
