b967236a9487266ce2a585308ad18bd85ea8de86
Hao <haozhi.dev@gmail.com>
2026-07-24 16:27:47 +0100

    pricing ix core

--- pricing/reserve-v2/core/src/pda.rs
+++ pricing/reserve-v2/core/src/pda.rs
@@ -8,19 +8,54 @@ use crate::internal_utils::const_map;
 use crate::keys::CONST_KEYS_OWNED;
 
 pub const PRICING_STATE_SEED: [u8; 1] = *b"p";
+pub const POOL_STATE_SEED: [u8; 5] = *b"state";
 
 pub const fn const_find_pricing_state(prog_id: &[u8; 32]) -> ([u8; 32], u8) {
     derive_program_address(&[&PRICING_STATE_SEED], prog_id)
+}
+
+pub const fn const_find_pool_state(prog_id: &[u8; 32]) -> ([u8; 32], u8) {
+    derive_program_address(&[&POOL_STATE_SEED], prog_id)
+}
+
+pub const fn ata_seeds<'a>(
+    auth: &'a [u8; 32],
+    token_prog: &'a [u8; 32],
+    mint: &'a [u8; 32],
+) -> [&'a [u8; 32]; 3] {
+    [auth, token_prog, mint]
+}
+
+pub const fn wsol_reserves_ata_seeds(pool_state: &[u8; 32]) -> [&[u8; 32]; 3] {
+    ata_seeds(
+        pool_state,
+        CONST_KEYS_OWNED.tokenkeg(),
+        CONST_KEYS_OWNED.wsol_mint(),
+    )
+}
+
+pub const fn const_find_wsol_reserves_ata(pool_state: &[u8; 32]) -> ([u8; 32], u8) {
+    let [s0, s1, s2] = wsol_reserves_ata_seeds(pool_state);
+    derive_program_address(
+        &[s0.as_slice(), s1.as_slice(), s2.as_slice()],
+        CONST_KEYS_OWNED.atoken(),
+    )
 }
 
 #[generic_array_struct(all pub)]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct ConstPdas<T> {
     pub pricing_state: T,
+    pub pool_state: T,
+    pub wsol_reserves: T,
 }
+
+const POOL_STATE: ([u8; 32], u8) = const_find_pool_state(CONST_KEYS_OWNED.reserve_v2_program());
 
 pub const CONST_PDAS: ConstPdas<([u8; 32], u8)> = ConstPdas::const_from_destr(ConstPdasDestr {
     pricing_state: const_find_pricing_state(CONST_KEYS_OWNED.program()),
+    pool_state: POOL_STATE,
+    wsol_reserves: const_find_wsol_reserves_ata(&POOL_STATE.0),
 });
 
 const fn const_pda_addr((pda, _): &([u8; 32], u8)) -> [u8; 32] {
@@ -61,6 +96,8 @@ mod tests {
             ConstPdas(
                 [
                     "8mvhAnVbCMyRU9mn9uH9ZwEeJGchZvBbHHecqHemtwRZ",
+                    "9zMRqtjkTvUm4kVtz2MrPiJnr9spUmYsr8Uqis7y3Brv",
+                    "6nBpYJ3oeraht4cFyFPj3TLFpNuy8SRMQA2KGRXVAEHY",
                 ],
             )
         "#]];
@@ -68,6 +105,8 @@ mod tests {
             ConstPdas(
                 [
                     255,
+                    254,
+                    254,
                 ],
             )
         "#]];
